6ccfdcf38564774f4f94f21cc762ed2ca8339cc0
Wojtasb102 <wojtasb102@gmail.com>
2021-11-03 21:40:52 +0100

    .

---
+++ PythonExcel/main.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="serwer2180298.home.pl",
+  user="34017600_wykaz",
+  password="k8VPpxzd",
+  database="34017600_wykaz"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM wykaz")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+#hasło do bazy ZaQ1@wSx
