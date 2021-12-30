3048fdb3d298e5adeda9160215cb176313adf43a
Wojtasb102 <wojtasb102@gmail.com>
2021-12-30 13:17:06 +0100

    s

--- pythonProgs/kwadraty.py
+++ pythonProgs/kwadraty.py
@@ -1,2 +1,15 @@
-napis
-sa
+import math
+
+
+class Zespolona:
+    def __init__ (self, re, im):
+        self.re = re
+        self.im = im
+
+    def modul(self):
+        mod = math.sqrt(self.re**2+self.im**2)
+        return mod
+
+
+image = Zespolona(3,2)
+print(image.modul())
