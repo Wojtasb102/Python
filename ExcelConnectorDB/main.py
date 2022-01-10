0d60d6c4e3d0de92efdde82c7e5757c215fe1984
Wojtasb102 <wojtasb102@gmail.com>
2022-01-10 22:15:00 +0100

    a
    a

--- ExcelConnectorDB/main.py
+++ ExcelConnectorDB/main.py
@@ -1,19 +1,25 @@
 from openpyxl import *
 import os
+import sqlite3
 
 
 class Connector:
-    def __init__(self, excelName):
+    def __init__(self, excelName, dbName):
         self.excelName = excelName
-        if not self.is_file():
+        self.dbName = dbName
+        self.dirname = os.path.dirname(__file__)
+        if not self.is_file(self.excelName):
             workbook = Workbook()
             workbook.save(filename=excelName)
         self.workbook = load_workbook(filename=self.excelName)
         self.worksheet = self.workbook.active
+        if not self.is_file(self.dbName):
+            filename = os.path.join(self.dirname, self.dbName)
+            conn = sqlite3.connect(filename)
 
-    def is_file(self):
-        dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, self.excelName)
+    def is_file(self, fileName):
+        #dirname = os.path.dirname(__file__)
+        filename = os.path.join(self.dirname, fileName)
         print(os.path.isfile(filename))
         return os.path.isfile(filename)
 
@@ -27,17 +33,22 @@ class Connector:
     def change_worksheet(self, sheetname):
         self.worksheet = self.workbook[sheetname]
 
+    def create_db_connection(self):
+        filename = os.path.join(self.dirname, self.dbName)
+        conn = sqlite3.connect(filename)
+
+
 
 def main():
     print("Start Main")
-    conector = Connector("Excel.xlsx")
-    conector.is_file()
+    conector = Connector("Excel.xlsx", "Database.db")
+    conector.is_file("Excel.xlsx")
+    conector.is_file("Database.db")
     print("Start Main")
     print(conector.sheet_list())
     conector.change_worksheet("Sheet")
     print(conector.read_cell( 1, 1))
     conector.change_worksheet("Arkusz1")
     print(conector.read_cell(1, 1))
-
 
 main()
