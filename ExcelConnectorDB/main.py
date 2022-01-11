9283b645bf1fe810bd49884d91b2866dd4b55246
Wojtasb102 <wojtasb102@gmail.com>
2022-01-11 21:22:51 +0100

    a
    a

--- ExcelConnectorDB/main.py
+++ ExcelConnectorDB/main.py
@@ -18,7 +18,7 @@ class Connector:
             conn = sqlite3.connect(filename)
 
     def is_file(self, fileName):
-        #dirname = os.path.dirname(__file__)
+        # dirname = os.path.dirname(__file__)
         filename = os.path.join(self.dirname, fileName)
         print(os.path.isfile(filename))
         return os.path.isfile(filename)
@@ -26,9 +26,9 @@ class Connector:
     def sheet_list(self):
         return self.workbook.sheetnames
 
-    def read_cell(self, x, y):
-#        return self.worksheet.cell(row=x, column=y).number_format
-        return self.worksheet.cell(row=x, column=y).value
+    def get_cell(self, x, y):
+        #        return self.worksheet.cell(row=x, column=y).number_format
+        return self.worksheet.cell(row=y, column=x).value
 
     def change_worksheet(self, sheetname):
         self.worksheet = self.workbook[sheetname]
@@ -37,6 +37,22 @@ class Connector:
         filename = os.path.join(self.dirname, self.dbName)
         conn = sqlite3.connect(filename)
 
+    def get_dimension(self):
+        dimension = (self.worksheet.max_row, self.worksheet.max_column)
+        return dimension
+
+    def get_header(self):
+        header = []
+        for i in range(1, self.get_dimension()[1] + 1):
+            header.append(self.get_cell(i, 1))
+        return header
+
+    def print_data(self):
+        i = 1
+        for x in self.get_header():
+            for y in range(2, self.get_dimension()[0] + 1):
+                print(self.get_cell(i, y))
+            i = i + 1
 
 
 def main():
@@ -47,8 +63,13 @@ def main():
     print("Start Main")
     print(conector.sheet_list())
     conector.change_worksheet("Sheet")
-    print(conector.read_cell( 1, 1))
+    print(conector.get_cell(1, 1))
     conector.change_worksheet("Arkusz1")
-    print(conector.read_cell(1, 1))
+    print(conector.get_cell(1, 1))
+    conector.change_worksheet("Sheet")
+    print(conector.get_dimension()[0])
+    print(conector.get_header())
+    conector.print_data()
+
 
 main()
